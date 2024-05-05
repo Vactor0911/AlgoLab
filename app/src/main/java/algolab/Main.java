cfad64a7d6d574dec7ca8150d51d8e6f8639e01c
Vactor0911 <vactor0911@gmail.com>
2024-05-06 03:58:31 +0900

    Edited main

--- app/src/main/java/algolab/Main.java
+++ app/src/main/java/algolab/Main.java
@@ -1,9 +1,21 @@
 package algolab;
 
+import java.net.URL;
+
 public class Main {
 
     public static void main(String[] args) {
         new Frame();
+    }
+    
+    /**
+     * 리소스의 상대 경로를 입력받아 절대 경로를 반환한다.
+     * @param path - 원하는 리소스의 파일 경로
+     * @return {@link java.net.URL} 객체; 경로에서 찾을 수 있는 리소스가 없을 경우 {@code null} 반환
+     * @throws NullPointerException {@code path}가 {@code null}인 경우
+     */
+    public static URL getPath(String path) {
+        return Main.class.getResource(path);
     }
     
 }
