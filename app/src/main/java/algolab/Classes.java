317346effe6c4c582668b32ed3dce4d9b0cc76d4
Vactor0911 <vactor0911@gmail.com>
2024-05-10 02:00:49 +0900

    Added GbcFactory class & VerticalLayout class

--- app/src/main/java/algolab/Classes.java
+++ app/src/main/java/algolab/Classes.java
@@ -1,5 +1,139 @@
 package algolab;
 
-public class Classes {
-    
+import java.awt.*;
+
+class GbcFactory {
+    private static GridBagConstraints gbc = new GridBagConstraints();
+
+    /**
+     * 주어진 속성으로 설정된 {@code GridBagConstraints} 객체를 반환한다.
+     * @param x 객체가 위치할 열 번호
+     * @param y 객체가 위치할 행 번호
+     * @param weightX 열이 차지할 비율
+     * @param weightY 행이 차지할 비율
+     * @param width 객체가 차지할 열 개수
+     * @param height 객체가 차지할 행 개수
+     * @return {@code GridBagConstraints} 객체
+     */
+    public static GridBagConstraints createGbc(int x, int y, double weightX, double weightY, int width, int height) {
+        gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.weightx = weightX;
+        gbc.weighty = weightY;
+        gbc.gridwidth = width;
+        gbc.gridheight = height;
+        return gbc;
+    }
+
+    /**
+     * 주어진 속성으로 설정된 {@code GridBagConstraints} 객체를 반환한다.
+     * @param x 객체가 위치할 열 번호
+     * @param y 객체가 위치할 행 번호
+     * @param weightX 열이 차지할 비율
+     * @param weightY 행이 차지할 비율
+     * @return {@code GridBagConstraints} 객체
+     */
+    public static GridBagConstraints createGbc(int x, int y, double weightX, double weightY) {
+        return createGbc(x, y, weightX, weightY, 1, 1);
+    }
+
+    /**
+     * 주어진 속성으로 설정된 {@code GridBagConstraints} 객체를 반환한다.
+     * @param x 객체가 위치할 열 번호
+     * @param y 객체가 위치할 행 번호
+     * @param width 객체가 차지할 열 개수
+     * @param height 객체가 차지할 행 개수
+     * @return {@code GridBagConstraints} 객체
+     */
+    public static GridBagConstraints createGbc(int x, int y, int width, int height) {
+        return createGbc(x, y, 1, 1, width, height);
+    }
+
+    /**
+     * 주어진 속성으로 설정된 {@code GridBagConstraints} 객체를 반환한다.
+     * @param x 객체가 위치할 열 번호
+     * @param y 객체가 위치할 행 번호
+     * @return {@code GridBagConstraints} 객체
+     */
+    public static GridBagConstraints createGbc(int x, int y) {
+        return createGbc(x, y, 1, 1, 1, 1);
+    }
+}
+
+/**
+ * 객체를 세로로 배치할 수 있는 배치 관리자이다.
+ */
+class VerticalLayout implements LayoutManager {
+    private final Dimension MINIMUM_SIZE = new Dimension();
+    private int gapY;
+
+    /**
+     * 객체를 세로로 배치할 수 있는 배치 관리자이다.
+     * @param gapY 객체 사이의 간격
+     */
+    public VerticalLayout(int gapY) {
+        super();
+        this.gapY = gapY;
+    }
+    /**
+     * 객체를 세로로 배치할 수 있는 배치 관리자이다.
+     */
+    public VerticalLayout() {
+        this(0);
+    }
+
+    @Override
+    public void addLayoutComponent(String name, Component comp) {
+    }
+
+    @Override
+    public void removeLayoutComponent(Component comp) {
+    }
+
+    @Override
+    public Dimension preferredLayoutSize(Container parent) {
+        Dimension dimension = new Dimension( getTotalPaddingWidth(parent), getTotalPaddingHeight(parent) );
+        MINIMUM_SIZE.height = dimension.height;
+        for(Component c :parent.getComponents()){
+            if( !c.isVisible() ){
+                continue;
+            }
+
+            Dimension preferredSize = c.getPreferredSize();
+            dimension.height += preferredSize.getHeight();
+            if(preferredSize.width > dimension.width){
+                dimension.width = preferredSize.width;
+            }
+        }
+        MINIMUM_SIZE.width = dimension.width;
+        return dimension;
+    }
+
+    private int getTotalPaddingWidth(Container container) {
+        Insets padding = container.getInsets();
+        return padding.left + padding.right;
+    }
+    private int getTotalPaddingHeight(Container container){
+        Insets padding = container.getInsets();
+        return padding.top + padding.bottom;
+    }
+
+    @Override
+    public Dimension minimumLayoutSize(Container parent) {
+        return MINIMUM_SIZE;
+    }
+
+    @Override
+    public void layoutContainer(Container parent) {
+        Insets padding = parent.getInsets();
+        int y = padding.top;
+        for( Component c : parent.getComponents() ){
+            if( c.isVisible() ) {
+                c.setBounds(padding.left, y, parent.getWidth(), c.getPreferredSize().height);
+                y += c.getHeight() + gapY;
+           }
+        }
+    }
 }
