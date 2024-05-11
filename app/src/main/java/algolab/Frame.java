ae875afea5ed3537375647c456fbde9387e77a17
Vactor0911 <vactor0911@gmail.com>
2024-05-11 21:54:02 +0900

    Added basic frame interface

--- app/src/main/java/algolab/Frame.java
+++ app/src/main/java/algolab/Frame.java
@@ -1,17 +1,35 @@
 package algolab;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class Frame extends JFrame {
+    private JPanel pnlMenu = new JPanel();
+    private JPanel pnlContent = new JPanel();
+
+    private Button btnLearning = new Button("Learning");
+    private Button btnTraining = new Button("Training");
+    private Button btnQuiz = new Button("Quiz");
 
     public Frame() {
         setTitle("AlgoLab");
         setSize(600, 400);
+        setMinimumSize( new Dimension(600, 400) );
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
         setLocationRelativeTo(null);
-        
-        repaint();
+
+        setLayout( new GridBagLayout() );
+
+        //메뉴 패널
+        pnlMenu.setLayout( new GridLayout(3, 1) );
+        pnlMenu.setBackground(Color.LIGHT_GRAY);
+        add( pnlMenu, GbcFactory.createGbc(0, 0, 0.2d, 1.0d) );        
+
+        //내용 패널
+        pnlContent.setLayout( new BorderLayout() );
+        add( pnlContent, GbcFactory.createGbc(1, 0, 0.8d, 1.0d) );
+
+        setVisible(true);
     }
 
 }
