ce469c0c217b8c5761b33886a6f0de50d0a1d8f9
costar011 <dnaer456@gmail.com>
2024-05-17 23:54:31 +0900

    프레임 화면 구성 , 스크린 라디오버튼 구성

--- app/src/main/java/algolab/Frame.java
+++ app/src/main/java/algolab/Frame.java
@@ -1,7 +1,8 @@
 package algolab;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.*; 
+import javax.swing.*; 
+import java.awt.event.*; 
 
 public class Frame extends JFrame {
     private JPanel pnlMenu = new JPanel();
@@ -10,6 +11,9 @@ public class Frame extends JFrame {
     private Button btnLearning = new Button("Learning");
     private Button btnTraining = new Button("Training");
     private Button btnQuiz = new Button("Quiz");
+    JRadioButton jRadioButton1; 
+    JRadioButton jRadioButton2; 
+    JButton jButton;
 
     // Yerim2
 
@@ -21,6 +25,10 @@ public class Frame extends JFrame {
         setLocationRelativeTo(null);
 
         setLayout( new GridBagLayout() );
+
+        jRadioButton1 = new JRadioButton();
+        jRadioButton2 = new JRadioButton(); 
+        jButton = new JButton("Click");
 
         //메뉴 패널
         pnlMenu.setLayout( new GridLayout(3, 1) );
@@ -34,23 +42,35 @@ public class Frame extends JFrame {
         pnlContent.setLayout( new GridBagLayout() );
 
         // 1행
-        pnlContent.add(new JLabel(""), GbcFactory.createGbc(0, 0, 1d, 0.3d, 3, 1));
+        pnlContent.add(new JLabel("1. bubble"), GbcFactory.createGbc(0, 0, 0.16d, 0.2d));  
+        pnlContent.add(new JLabel(""), GbcFactory.createGbc(1, 0, 0.16d, 0.2d));  
+        pnlContent.add(new JLabel(""), GbcFactory.createGbc(2,0, 0.16d, 0.2d));
+        pnlContent.add(new JLabel(""), GbcFactory.createGbc(3, 0, 0.16d, 0.2d));
+        pnlContent.add(new JLabel("1/5 문제"), GbcFactory.createGbc(4, 0, 0.16d, 0.2d));
         
         //2행
         pnlContent.add(new JLabel(""), GbcFactory.createGbc(0, 1, 0.33d, 0.2d));  
-        pnlContent.add(new JLabel("Quiz", SwingConstants.CENTER), GbcFactory.createGbc(1, 1,0.34d, 0.2d));  
+        pnlContent.add(new JLabel(""), GbcFactory.createGbc(1, 1, 0.33d, 0.2d));  
         pnlContent.add(new JLabel(""), GbcFactory.createGbc(2, 1, 0.33d, 0.2d));
+        pnlContent.add(new JLabel(""), GbcFactory.createGbc(3, 1, 0.33d, 0.2d));  
+        pnlContent.add(new JLabel(""), GbcFactory.createGbc(4, 1, 0.33d, 0.2d));  
+    
 
         //3행
-        pnlContent.add(new JLabel(""), GbcFactory.createGbc(0, 2, 1d, 0.3d, 3, 1));  
+        pnlContent.add(new JLabel(""), GbcFactory.createGbc(0, 2, 1d, 0.3d, 3, 1));
+        pnlContent.add(new JLabel(""), GbcFactory.createGbc(1, 2, 1d, 0.3d, 3, 1));    
+        pnlContent.add(new JLabel(""), GbcFactory.createGbc(3, 2, 1d, 0.3d, 3, 1));    
+        pnlContent.add(new JLabel(""), GbcFactory.createGbc(4, 2, 1d, 0.3d, 3, 1));    
+    
 
         //4행
         pnlContent.add(new JLabel(""), GbcFactory.createGbc(0, 3, 0.33d, 0.2d));  
-        pnlContent.add(new Button("Start"), GbcFactory.createGbc(1, 3,0.34d, 0.2d));  
+        pnlContent.add(new JLabel(""), GbcFactory.createGbc(1, 3,0.34d, 0.2d));  
         pnlContent.add(new JLabel(""), GbcFactory.createGbc(2, 3, 0.33d, 0.2d));
 
         //5행
         pnlContent.add(new JLabel(""), GbcFactory.createGbc(0, 4, 1d, 0.3d, 3, 1)); 
+        jRadioButton1.setText("Test");
         
         add( pnlContent, GbcFactory.createGbc(1, 0, 0.8d, 1.0d) );
         setVisible(true);
@@ -60,7 +80,7 @@ public class Frame extends JFrame {
         @Override
         public void add(Component component, Object constraints) {
             super.add(component, constraints);
-            //((JComponent)component).setBorder( BorderFactory.createLineBorder(Color.BLACK) );
+            ((JComponent)component).setBorder( BorderFactory.createLineBorder(Color.BLACK) );
         }
     }
 }
