86bd7113a8f83015a0cb071a5307d7008f9461c6
Vactor0911 <vactor0911@gmail.com>
2024-05-17 22:40:08 +0900

    Refactored screens

--- app/src/main/java/algolab/Screens.java
+++ app/src/main/java/algolab/Screens.java
@@ -4,91 +4,96 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Screens {
-
+    public JLabel getEmptyLabel() {
+        return new JLabel("");
+    }
 }
 
 class LearningScreen extends JPanel {
     // SeonJin
-    private String[] algo = { "Selection Sort", "Insertion Sort", "Bubble Sort", "Quick Sort", "Merge Sort" };
-    private JComboBox algoCombo = new JComboBox<>(algo);
-
-    private JButton btnLearn = new JButton("Go Learning");
-
+    private ComboBox comboAlgorithm = new ComboBox();
+    private Button btnLearn = new Button("실습하기");
     private JTabbedPane tabLearn = new JTabbedPane();
 
     public LearningScreen() {
-        setLayout(new GridBagLayout());
+        //초기화
+        comboAlgorithm.addItems(new String[][] {
+            {"버블 정렬", ""},
+            {"선택 정렬", ""},
+            {"삽입 정렬", ""},
+            {"퀵 정렬", ""},
+            {"병합 정렬", ""}
+        });
 
-        tabLearn.addTab("definition", new JLabel(""));
-        tabLearn.addTab("View Code", new JLabel(""));
-        tabLearn.addTab("Time Complexity", new JLabel(""));
+        //화면 구성
+        setLayout( new GridBagLayout() );
 
-        // 아!!!!!!!!!!!!!!!!!왜 안되냐고!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        for (String str : new String[] {"정의", "코드", "시간 복잡도"}) {
+            tabLearn.addTab( str, new JLabel() );
+        }
 
         // 1행
-        add(algoCombo, GbcFactory.createGbc(0, 0, 0.5d, 0.1d, 1, 1));
-        add(new JLabel(""), GbcFactory.createGbc(1, 0, 0.05d, 0.1d, 1, 1));
-        add(btnLearn, GbcFactory.createGbc(2, 0, 0.1d, 0.1d, 1, 1));
-        add(new JLabel(""), GbcFactory.createGbc(3, 0, 0.05, 0.1, 1, 1));
-        add(new JLabel(""), GbcFactory.createGbc(4, 0, 0.3d, 0.1, 1, 1));
+        add( comboAlgorithm, GbcFactory.createGbc(0, 0, 0.5d, 0.1d) );
+        add( new JLabel(""), GbcFactory.createGbc(1, 0, 0.05d, 0.1d) );
+        add( btnLearn, GbcFactory.createGbc(2, 0, 0.1d, 0.1d) );
+        add( new JLabel(""), GbcFactory.createGbc(3, 0, 0.05, 0.1) );
+        add( new JLabel(""), GbcFactory.createGbc(4, 0, 0.3d, 0.1) );
 
         // 2행
-        add(new JLabel(""), GbcFactory.createGbc(0, 1, 0.65d, 0.05d, 3, 1));
-        add(new JLabel(""), GbcFactory.createGbc(3, 1, 0.05d, 0.05d, 1, 1));
-        add(new JLabel(""), GbcFactory.createGbc(4, 1, 0.3d, 0.05d, 1, 1));
+        add( new JLabel(""), GbcFactory.createGbc(0, 1, 0.65d, 0.05d, 3, 1) );
+        add( new JLabel(""), GbcFactory.createGbc(3, 1, 0.05d, 0.05d) );
+        add( new JLabel(""), GbcFactory.createGbc(4, 1, 0.3d, 0.05d) );
 
         // 3행
-        add(tabLearn, GbcFactory.createGbc(0, 2, 0.65d, 0.85d, 3, 2));
-        add(new JLabel(""), GbcFactory.createGbc(3, 2, 0.05d, 0.4d, 1, 1));
-        add(new JLabel("table"), GbcFactory.createGbc(4, 2, 0.05d, 0.4d, 1, 1));
+        add( tabLearn, GbcFactory.createGbc(0, 2, 0.65d, 0.85d, 3, 2) );
+        add( new JLabel(""), GbcFactory.createGbc(3, 2, 0.05d, 0.4d) );
+        add( new JLabel("table"), GbcFactory.createGbc(4, 2, 0.05d, 0.4d) );
 
         // 4행
-        add(new JLabel(""), GbcFactory.createGbc(3, 3, 0.05d, 0.5d, 1, 1));
-        add(new JLabel("graph"), GbcFactory.createGbc(4, 3, 0.3d, 0.45d, 1, 1));
-
+        add( new JLabel(""), GbcFactory.createGbc(3, 3, 0.05d, 0.5d) );
+        add( new JLabel("graph"), GbcFactory.createGbc(4, 3, 0.3d, 0.45d) );
         // 사진은 넣으면 이상하게 변해서 Label로 대체했음
     }
 }
 
 class PracticeScreen extends JPanel {
-    private String[] algo = {"Selection Sort", "Insertion Sort", "Bubble Sort", "Quick Sort", "Merge Sort"};
-    private JComboBox algoCombo = new JComboBox<>(algo);
-    private JButton btnLearn = new JButton("Go Learning");
-    private JLabel grape = new JLabel("Grape");
-    private JTabbedPane tabLearn = new JTabbedPane();
+    private ComboBox comboAlgorithm = new ComboBox();
+    private Button btnLearn = new Button("학습하기");
+    private JLabel graph = new JLabel("막대 그래프");
+    private ListBox listBoxArray = new ListBox(1);
+    private JLabel controlPanel = new JLabel("조작 패널");
 
     public PracticeScreen() {
-        //내용 패널
-        setLayout( new GridBagLayout() );
-        tabLearn.addTab("definition", new JLabel(""));
-        tabLearn.addTab("View Code", new JLabel(""));
-        tabLearn.addTab("Time Complexity", new JLabel(""));
+        //초기화
+        comboAlgorithm.addItems(new String[][] {
+            {"버블 정렬", ""},
+            {"선택 정렬", ""},
+            {"삽입 정렬", ""},
+            {"퀵 정렬", ""},
+            {"병합 정렬", ""}
+        });
+        graph.setBorder( BorderFactory.createLineBorder(Color.BLACK) );
+        controlPanel.setBorder( BorderFactory.createLineBorder(Color.BLACK) );
 
-        grape.setBorder( BorderFactory.createLineBorder(Color.BLACK));
+        //화면 구성
+        setLayout( new GridBagLayout() );
 
         // 1행
-        add(algoCombo, GbcFactory.createGbc(0, 0, 0.2d, 0.1d,1,1));
-        add(new JButton("학습하기"), GbcFactory.createGbc(3, 0, 0.25d, 0.1d, 1, 1));  
-        add(new JLabel(""), GbcFactory.createGbc(2, 0, 0.02d, 0.02d, 1, 1));  
-        add(new JLabel(""), GbcFactory.createGbc(4, 0, 0.4d, 0.1d, 1, 1));  
-        add(new JLabel(""), GbcFactory.createGbc(8, 0, 0.01d, 0.01d, 1, 1));
+        add( comboAlgorithm, GbcFactory.createGbc(0, 0, 0.45d, 0.1d) );
+        add( new JLabel(""), GbcFactory.createGbc(1, 0, 0.1d, 0.1d) );
+        add( new JLabel(""), GbcFactory.createGbc(2, 0, 0.05d, 0.1d) );
+        add( btnLearn, GbcFactory.createGbc(3, 0, 0.2d, 0.1d) );
+        add( new JLabel(""), GbcFactory.createGbc(4, 0, 0.2d, 0.1d) );
 
         // 2행
-        add(new JLabel(""), GbcFactory.createGbc(0, 1, 0.25d, 0.08d, 1, 1));
-        add(new JLabel(""), GbcFactory.createGbc(1, 1, 0.25d, 0.08d, 1,1));
-        add(new JLabel(""), GbcFactory.createGbc(2, 1, 0.02d, 0.08d, 1, 1));
-        add(new JLabel(""), GbcFactory.createGbc(3, 1, 0.25d, 0.08d, 1, 1));
-        add(new JLabel(""), GbcFactory.createGbc(8, 1, 0.01d, 0.08d, 1, 1));
+        add( new JLabel(""), GbcFactory.createGbc(0, 1, 1.0d, 0.05d, 5, 1) );
         
         // 3행
-        add(grape, GbcFactory.createGbc(0, 2, 0.8d, 0.5d,2,5));
-        add(new ListBox(2), GbcFactory.createGbc(3, 2, 0.8d, 0.4d,3,3));
-        add(new JLabel(""), GbcFactory.createGbc(2, 2, 0.04d, 0.1d, 1, 5));
-        add(new JLabel(""), GbcFactory.createGbc(8, 2, 0.01d, 1d, 1, 1));
+        add( graph, GbcFactory.createGbc(0, 2, 0.55d, 0.85d,2,2) );
+        add( new JLabel(""), GbcFactory.createGbc(2, 2, 0.05d, 0.85d, 1, 2) );
+        add( listBoxArray, GbcFactory.createGbc(3, 2, 0.4d, 0.75d,2,1) );
 
         //4행
-        add(new JButton("실행하기"), GbcFactory.createGbc(3, 5, 0.01d, 0.02d, 3, 1));
-        add(new JLabel(""), GbcFactory.createGbc(8, 3, 0.01d, 0.01d, 1, 1));
-        add(new JLabel(""), GbcFactory.createGbc(8, 4, 0.01d, 1, 1, 1));
+        add( controlPanel, GbcFactory.createGbc(3, 3, 0.04d, 0.1d, 2, 1) );
     }
 }
